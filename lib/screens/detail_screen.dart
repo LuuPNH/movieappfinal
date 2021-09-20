aeabe67d0a6d9dd74bbcf418eb1db877701a4c9a
Hai Luu <hailuu.1999@gmail.com>
2021-09-21 03:01:29 +0700

    finished movie

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,10 +1,16 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:movieappfinal/bloc/get_movie_videos_bloc.dart';
 import 'package:movieappfinal/model/movie.dart';
 import 'package:movieappfinal/model/video.dart';
 import 'package:movieappfinal/model/video_response.dart';
+import 'package:movieappfinal/screens/video_player.dart';
 import 'package:movieappfinal/style/theme.dart' as Style;
-import 'package:sliver_fab/sliver_fab.dart';
+import 'package:movieappfinal/widgets/casts.dart';
+import 'package:movieappfinal/widgets/movie_info.dart';
+import 'package:movieappfinal/widgets/similar_movies.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class MovieDetailScreen extends StatefulWidget {
   final Movie movie;
@@ -31,32 +37,40 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
 
   @override
   Widget build(BuildContext context) {
+    bool _pinned = true;
+    bool _snap = false;
+    bool _floating = false;
     return Scaffold(
-      backgroundColor: Style.Colors.mainColor,
-      body: Builder(builder: (context) {
-        return SliverFab(
-          floatingPosition: FloatingPosition(right: 20.0),
-          floatingWidget: StreamBuilder<VideoResponse>(
-            stream: movieVideosBloc.subject.stream,
-            builder: (context, AsyncSnapshot<VideoResponse> snapshot) {
-              if (snapshot.hasData) {
-                if (snapshot.data!.error.length > 0) {
-                  return _buildErrorWidget();
-                }
-                return _buildHomeWidget(snapshot.data!);
-              } else if (snapshot.hasError) {
-                return _buildErrorWidget();
-              } else {
-                return _buildLoadingWidget();
-              }
-            },
-          ),
-          expandedHeight: 200.0,
+        backgroundColor: Style.Colors.mainColor,
+        body: CustomScrollView(
           slivers: [
             SliverAppBar(
+              pinned: _pinned,
+              snap: _snap,
+              floating: _floating,
               backgroundColor: Style.Colors.mainColor,
               expandedHeight: 200.0,
-              pinned: true,
+              bottom: PreferredSize(
+                  preferredSize: const Size.fromHeight(0.0),
+                  child: Transform.translate(
+                    offset: const Offset(150, 30),
+                    child: StreamBuilder<VideoResponse>(
+                      stream: movieVideosBloc.subject.stream,
+                      builder:
+                          (context, AsyncSnapshot<VideoResponse> snapshot) {
+                        if (snapshot.hasData) {
+                          if (snapshot.data!.error.length > 0) {
+                            return _buildErrorWidget();
+                          }
+                          return _buildHomeWidget(snapshot.data!);
+                        } else if (snapshot.hasError) {
+                          return _buildErrorWidget();
+                        } else {
+                          return _buildLoadingWidget();
+                        }
+                      },
+                    ),
+                  )),
               flexibleSpace: FlexibleSpaceBar(
                 title: Text(
                   movie.title.length > 40
@@ -75,7 +89,7 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
                                       movie.backPoster))),
                       child: Container(
                         decoration:
-                            BoxDecoration(color: Colors.black.withOpacity(0.5)),
+                            BoxDecoration(color: Colors.black.withOpacity(0.3)),
                       ),
                     ),
                     Container(
@@ -91,11 +105,80 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
                   ],
                 ),
               ),
-            )
+            ),
+            SliverPadding(
+                padding: EdgeInsets.all(0.0),
+                sliver: SliverList(
+                    delegate: SliverChildListDelegate(
+                  [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10.0, top: 20.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            (movie.rating / 2).toString().substring(0, 3),
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          RatingBar.builder(
+                            itemSize: 10.0,
+                            initialRating: movie.rating / 2,
+                            minRating: 1,
+                            direction: Axis.horizontal,
+                            allowHalfRating: true,
+                            itemCount: 5,
+                            itemPadding: EdgeInsets.symmetric(horizontal: 2.0),
+                            itemBuilder: (context, _) => Icon(
+                              EvaIcons.star,
+                              color: Style.Colors.secondColor,
+                            ),
+                            onRatingUpdate: (rating) {
+                              print(rating);
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10.0, top: 20.0),
+                      child: Text(
+                        "OVERVIEW",
+                        style: TextStyle(
+                            color: Style.Colors.titleColor,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12.0),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        movie.overview,
+                        style: TextStyle(
+                            color: Colors.white, fontSize: 12.0, height: 1.5),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    MovieInfo(
+                      id: movie.id,
+                    ),
+                    SimilarMovies(
+                      id: movie.id,
+                    )
+                  ],
+                )))
           ],
-        );
-      }),
-    );
+        ));
   }
 
   Widget _buildLoadingWidget() {
@@ -113,8 +196,24 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
   Widget _buildHomeWidget(VideoResponse data) {
     List<Video> videos = data.videos;
     return FloatingActionButton(
-        backgroundColor: Style.Colors.secondColor,
-        child: Icon(Icons.play_arrow),
-        onPressed: null);
+      backgroundColor: Style.Colors.secondColor,
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => VideoPlayerScreen(
+              controller: YoutubePlayerController(
+                initialVideoId: videos[0].key,
+                flags: YoutubePlayerFlags(
+                  autoPlay: true,
+                  mute: true,
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+      child: Icon(Icons.play_arrow),
+    );
   }
 }
